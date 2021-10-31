3850d793b02a70e5d8fe28fe9ed7efdaf56a8f9e
elza <elza>
2021-10-31 22:40:18 +0200

    add category functionality in admin dashboard

---
+++ resources/views/admin/category/add.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h4>Add category</h4>
+        </div>
+        <div class="card-body">
+        <form action="{{ url('insert-category')}}" method="POST" enctype="multipart/form-data">
+            @csrf
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label for="">Name</label>
+                        <input type="text" class="form-control" name="name">
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="">URL</label>
+                        <input type="text" class="form-control" name="url">
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label for="">Description</label>
+                        <textarea name="description" rows="3" class="form-control"></textarea>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label for="">Status</label>
+                        <input type="checkbox" name="status">
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label for="">Meta Title</label>
+                        <input type="text" name="meta_title" class="form-control">
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label for="">Meta Keywords</label>
+                        <textarea name="meta_keywords" rows="3" class="form-control"></textarea>
+                    </div>
+                    <div class="col-md-12 mb-3">
+                        <label for="">Meta Description</label>
+                        <textarea name="meta_description" rows="3" class="form-control"></textarea>
+                    </div>
+                    <div class="col-md-12">
+                        <input type="file" name="image" class="form-control">
+                    </div>
+                    <div class="col-md-12">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="card-body">
+        <h1>Category page</h1>
+    </div>
+@endsection
