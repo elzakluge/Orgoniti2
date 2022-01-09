705a9d26e9632e4b25831ca7f07d9a3c54ef9ba5
elza <elza>
2022-01-09 02:34:29 +0200

    last

--- resources/views/layouts/inc/adminnav.blade.php
+++ resources/views/layouts/inc/adminnav.blade.php
@@ -4,6 +4,11 @@
       <h6 class="font-weight-bolder mb-0">Dashboard</h6>
     </nav>
     <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+      <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+        <div class="input-group input-group-outline">
+          <label class="form-label">Type here...</label>
+          <input type="text" class="form-control">
+        </div>
       </div>
       <ul class="navbar-nav  justify-content-end">
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
@@ -13,6 +18,11 @@
                 {{ Auth::user()->name }}
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                  <a class="dropdown-item" href="#">
+                      My Profile
+                  </a>
+             </li>
              <li>
               <a class="dropdown-item" href="/">
                   Store
